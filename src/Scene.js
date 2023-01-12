1248a0819a98592dc25fa20d2152266e2e743d89
Jonath-z <jonathanzihindula95@gmail.com>
2023-01-12 10:12:54 +0200

    add style and className as props

--- src/Scene.js
+++ src/Scene.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Scene({ data = {}, children }) {
-  return <div {...data}>{children}</div>
+function Scene({ data = {}, children, className = '', style = {} }) {
+  return (
+    <div {...data} className={className} style={style}>
+      {children}
+    </div>
+  )
 }
 
 Scene.propTypes = {
-  data: PropTypes.object
+  data: PropTypes.object,
+  className: PropTypes.string,
+  style: PropTypes.object
 }
 
 export default Scene
