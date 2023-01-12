a87ce898da0bfeacc6e5f4a0787b4cbd36d927fb
Jonath-z <jonathanzihindula95@gmail.com>
2023-01-12 10:12:35 +0200

    implement the demo

--- example/src/App.js
+++ example/src/App.js
@@ -3,13 +3,9 @@ import './App.css'
 import { ScrollProvider, Scene } from 'react-z-scroll'
 
 const data = {
-  'data-0p': 'background: blue;',
-  'data-20p': '',
-  'data-50p': 'background: black;',
-  'data-70p': '',
-  'data-90p': 'background: red;',
-  'data-100p': ''
-  // 'data-1000p': ' tranform: tranlateY(-200px)'
+  'data-top-top': 'transform: translateX(-100%);',
+  'data-center-center': 'opacity: 1;transform: translateX(0%);',
+  'data-bottom-top': 'opacity: 0;'
 }
 
 function App() {
@@ -17,21 +13,73 @@ function App() {
     <div className='App'>
       <ScrollProvider
         initProps={{
-          constants: {
-            box: '300p'
-          }
+          smoothScrollingDuration: 1000,
+          smoothScrolling: true,
+          forceHeight: false
         }}
       >
         <Scene data={data}>
-          <div
-            style={{
-              height: '100vh',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%'
-            }}
-          ></div>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
+        </Scene>
+        <Scene data={data}>
+          <span>Start scrolling to see some magic happen {'\u2728'}</span>
         </Scene>
       </ScrollProvider>
     </div>
